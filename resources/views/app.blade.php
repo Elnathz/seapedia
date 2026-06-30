7aea82760d87235c6c121b951ae4a989331fadeb
Elnath <farrosrifantiarno32@gmail.com>
2026-06-30 22:13:13 +0700

    fix(build): remove vite fonts plugin to prevent docker build timeout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,8 @@
 
         <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml">
 
-        @fonts
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600&display=swap" rel="stylesheet" />
 
         @vite(['resources/css/app.css', 'resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         <x-inertia::head>
