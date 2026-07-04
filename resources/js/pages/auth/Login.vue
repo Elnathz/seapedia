2c3365bac33f5b36117deaa0e21d7faa785ea7b5
Elnath <farrosrifantiarno32@gmail.com>
2026-07-04 10:30:30 +0700

    chore(ui): use a generic email placeholder on login

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -50,7 +50,7 @@ defineProps<{
                     autofocus
                     :tabindex="1"
                     autocomplete="email"
-                    placeholder="email@kampus.ac.id"
+                    placeholder="email@gmail.com"
                 />
                 <InputError :message="errors.email" />
             </div>
